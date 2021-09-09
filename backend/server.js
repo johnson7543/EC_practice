8f3a07c65eb708ca7cca06aa3bd5986a5420e796
kaochx <tsubxxx@gmail.com>
2021-09-09 17:16:42 +0800

    ok

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,16 @@ import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
+
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
 const url = "mongodb+srv://johnson7543:jo10055096@cluster0.ciubh.mongodb.net/EC_practice?retryWrites=true&w=majority";
 mongoose.connect(url, {
   useNewUrlParser: true,
