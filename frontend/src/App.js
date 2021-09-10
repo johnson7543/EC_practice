b76f0b6754c09e3f3cc406914a5fb531f01c0520
kaochx <tsubxxx@gmail.com>
2021-09-10 09:28:15 +0800

    cart part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <a href="/">戶外用品店</a>
-        </div>
-        <div className="header-links">
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              戶外用品店
+            </Link>
+          </div>
+        <div>
+          <Link to="/cart">
+            Cart
+            {cartItems.length > 0 && (
+              <span className="badge">{cartItems.length}</span>
+            )}
+          </Link>
+          <Link to="/signin">Sign In</Link>
         </div>
       </header>
-      <aside>shopping categories</aside>
       <main>
+        <Route path="/cart/:id?" component={CartScreen}></Route>
         <Route path="/product/:id" component={ProductScreen}></Route>
         <Route path="/" component={HomeScreen} exact></Route>
       </main>
