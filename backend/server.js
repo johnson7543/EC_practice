84a75901512a93c0b69b89323344d9ca37647eb5
YuKai <johnson7543@cycu.org.tw>
2021-09-10 11:49:25 +0800

    paypal done

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const url = "mongodb+srv://johnson7543:jo10055096@cluster0.ciubh.mongodb.net/EC_practice?retryWrites=true&w=majority";
+const url =
+  'mongodb+srv://johnson7543:jo10055096@cluster0.ciubh.mongodb.net/EC_practice?retryWrites=true&w=majority';
 mongoose.connect(url, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
